b7c4d958ebdb58066d7447b93089e2974c5ccd3b
KaunainRizvi <krizvibusiness.com>
2024-02-19 00:27:11 +0530

    home css added

--- app_one/src/Components/Home.jsx
+++ app_one/src/Components/Home.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import vg from "../assets/2.webp"
 
 const Home = () => {
   return (
+
+    <>
     <div className='home'>
         
         <main>
             <h1>Tech Boom</h1>
+            <p>Solution to all your problems</p>
         </main>
     </div>
+
+    <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div> 
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi dignissimos nostrum nobis, animi quod similique veniam atque nemo molestiae commodi, deserunt numquam. Nesciunt iste architecto modi laboriosam obcaecati sequi sit.</p>
+
+        </div>
+    </div>
+
+    </>
   )
 }
 
