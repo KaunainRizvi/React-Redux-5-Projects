23097a7a4b175050f9943aae91ee0c7738af1225
KaunainRizvi <krizvibusiness.com>
2024-02-19 16:39:35 +0530

    app two initialized

---
+++ app_two_using_chakra_ui/src/App.js
@@ -0,0 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Header from './components/Header';
+
+function App() {
+  return (
+
+    <Router>
+      <Header/>
+   <Routes>
+
+    <Route></Route>
+   </Routes>
+
+    </Router>
+  )
+}
+
+export default App;
