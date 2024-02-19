ba5403426dc362c50bf02d374a23ef224bfd6bdb
KaunainRizvi <krizvibusiness.com>
2024-02-19 19:41:42 +0530

    sidebar full completed

--- app_two_using_chakra_ui/src/components/Header.jsx
+++ app_two_using_chakra_ui/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Button, useDisclosure} from "@chakra-ui/react"
+import {Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Button, useDisclosure, VStack, HStack} from "@chakra-ui/react"
 import { Link } from 'react-router-dom';
 import { BiMenuAltLeft} from "react-icons/bi"
 
@@ -16,7 +16,38 @@ const Header = () => {
     <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
         <DrawerOverlay/>
         <DrawerContent>
+        <DrawerCloseButton/>
+        <DrawerHeader>Video Guru</DrawerHeader>
+        <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+                <Button onClick={onClose} variant={"ghost"} colorScheme='purple'>
+                    <Link to={"/"}>Home</Link>
+                </Button>
 
+                <Button onClick={onClose} variant={"ghost"} colorScheme='purple'>
+                    <Link to={"/videos"}>Videos</Link>
+                </Button>
+
+                <Button onClick={onClose} variant={"ghost"} colorScheme='purple'>
+                    <Link to={"/videos?category=free"}>Free Videos</Link>
+                </Button>
+
+                <Button onClick={onClose} variant={"ghost"} colorScheme='purple'>
+                    <Link to={"/upload"}>Upload Video</Link>
+                </Button>
+            </VStack>
+
+          <HStack pos={'absolute'} bottom={"10"} left={"0"} w={"full"}
+            justifyContent={'space-evenly'}>
+            <Button onClick={onClose} colorScheme='purple'>
+                <Link to={"/login"}>Log In</Link>
+            </Button>
+
+            <Button onClick={onClose} colorScheme='purple' variant={"outline"} >
+                <Link to={"/signup"}>Sign Up</Link>
+            </Button>
+          </HStack>
+        </DrawerBody>
         </DrawerContent>
 
     </Drawer>
