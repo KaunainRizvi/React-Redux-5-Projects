fd66db07a50f726c102287aff68e3f497c0c7aee
KaunainRizvi <krizvibusiness.com>
2024-02-24 04:09:25 +0530

    videos not added yet

--- app_two_using_chakra_ui/src/components/Header.jsx
+++ app_two_using_chakra_ui/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <>
     <Button pos={"fixed"} top={"4"} left={"4"} colorScheme='purple' p={0} w={10} h={"10"}
-    borderRadius={"full"} onClick={onOpen}>
+    borderRadius={"full"} onClick={onOpen} zIndex={"overlay"}>
         <BiMenuAltLeft size={"20"}/>
     </Button>
 
