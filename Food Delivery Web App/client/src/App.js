6d3b8c9cbbb7973d0037dd8539f19f128d599903
KaunainRizvi <krizvibusiness.com>
2024-02-27 02:21:13 +0530

    creating home page

--- Food Delivery Web App/client/src/App.js
+++ Food Delivery Web App/client/src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Home from './screens/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './screens/Login';
 
 function App() {
   return (
     <>
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
+    </Router>
     </>
   );
 }
